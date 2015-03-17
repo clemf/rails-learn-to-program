9387115a29e9caceb25723e2539327520c2d5f5e
Clem Freeman and Joey Sandine <jasandine@gmail.com>
2015-03-17 11:09:57 -0700

    Add new lesson view

--- app/controllers/lessons_controller.rb
+++ app/controllers/lessons_controller.rb
@@ -1,5 +1,15 @@
 class LessonsController < ApplicationController
+
   def index
     @lessons = Lesson.all
   end
+
+  def show
+    @lesson = Lesson.find(params[:id])
+  end
+
+  def new
+    @lesson = Lesson.new
+  end
+  
 end
