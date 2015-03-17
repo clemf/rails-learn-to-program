c4cd88b1f0b7608c5d74dd67a1b19585b3475df9
Clem Freeman and Joey Sandine <clemfree@gmail.com>
2015-03-17 12:24:30 -0700

    Add next method to lesson

--- app/models/lesson.rb
+++ app/models/lesson.rb
@@ -1,3 +1,10 @@
 class Lesson < ActiveRecord::Base
   validates :name, :presence => true
+
+  def next
+    current_lesson = self
+    lesson_number = current_lesson.number + 1
+    next_lesson = Lesson.find_by number: lesson_number
+    return next_lesson
+  end
 end
